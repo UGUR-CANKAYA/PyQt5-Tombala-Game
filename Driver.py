9fe72cae78af745b3a862280a7717ccf352f8738
UGUR CANKAYA <42012189+UGUR-CANKAYA@users.noreply.github.com>
2022-08-07 14:10:16 +0300

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -1,5 +1,18 @@
+"""
+Tombala Game Driver Code
+
+@file: Driver.py
+@author: Uğur Çankaya
+@date: June 3, 2020
+@brief: Tombala Game with Qt Designer and Python
+
+OBJECT-ORIENTED PROGRAMMING II Project
+Eskişehir Osmangazi University Electrical and Electronics Engineering https://eee.ogu.edu.tr/en
+
+"""
+
 from PyQt5 import QtWidgets
-from Design import Ui_Form #"Design" oluşturduğumuz gui tasarım py dosyası
+from Design import Ui_Form #"Design" making gui design py file
 from Player import *
 import sys
 
